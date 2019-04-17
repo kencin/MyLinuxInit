ab1f1e8af6d73a1584ad81a72eeb911074ce8f49
Kencin <myzincx@gmail.com>
2019-04-17 16:48:51 +0800

    Update ss_bbr.sh

--- ss_bbr.sh
+++ ss_bbr.sh
@@ -5,9 +5,8 @@ export PATH
 #=================================================
 #       System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #       Description: Install the ShadowsocksR mudbjson server
-#       Version: 1.0.26
-#       Author: Toyo
-#       Blog: https://doub.io/ss-jc60/
+#       Author: Kencin
+#       PreAuthor: Toyo
 #=================================================
 
 sh_ver="1.0.26"
